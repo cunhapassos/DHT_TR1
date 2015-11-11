2665f6262267e6cdb3e6e14fe2a7a5e4e88890ae
cunhapassos <cunha.passos@gmail.com>
2015-11-11 12:24:54 -0200

    Comentários
    Inclusão de comentários no código

--- clienteUDP.py
+++ clienteUDP.py
@@ -1,19 +1,19 @@
 import socket
 
 def Main():
-    host = '127.0.0.1'
-    port = 5001
+    host = '127.0.0.1'	#Endereco IP do Servidor
+    port = 5001			#Porta que o Servidor esta
 
     server = ('127.0.0.1',5000)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind((host, port))
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	#Define o tipo da familia do protocolo e o tipo de Socket, nesse caso, UDP
+    s.bind((host, port))	#Esperara conexoes no endereco e porta fornecidos
 
     message = raw_input("-> ")
     while message != 'q':
         s.sendto(message, server)
         data, addr = s.recvfrom(1024)
-        print 'Received from server: ' + str(data)
+        print 'Recebido pelo servidor: ' + str(data)
         message = raw_input("-> ")
     s.close()
 
--- servidorUDP.py
+++ servidorUDP.py
@@ -1,22 +1,23 @@
 import socket
 
 def Main():
-    host = '127.0.0.1'
-    port = 5000
+    host = '127.0.0.1' 	#Endereco IP do Servidor
+    port = 5000			#Porta que o Servidor esta
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Define o tipo da familia do protocolo e o tipo de Socket, nesse caso, UDP
     s.bind((host,port))
 
 
-    print "Server Started."
+    print "Servidor Iniciado."
     while True:
-        data, addr = s.recvfrom(1024)
-        print "message From: " + str(addr)
-        print "from connected user: " + str(data)
+        data, addr = s.recvfrom(1024)	 #Recebe um dado de ate 1024 bytes retorna tambem o endereco de quem enviou
+        print "mensagem de: " + str(addr)
+        print "Do usuario conectado: " + str(data)
         data = str(data).upper()
-        print "sending: " + str(data)
-        s.sendto(data, addr)
+        print "Enviado: " + str(data)
+        s.sendto(data, addr) # retorna mensagem em maisculo para cliente
     c.close()
 
 if __name__ == '__main__':
     Main()
+
